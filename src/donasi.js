0782867e09d22c3de1d61b5d2ed67b6f4adeaa2d
KingIdBot <77149298+KingIdBot@users.noreply.github.com>
2021-01-14 20:18:24 +0800

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,19 @@
+const donasi = () => {
+	return `
+
+┏━━━━━━━━━━━━━━━━━━━━
+┃  𝗗𝗢𝗡𝗔𝗦𝗜  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ 𝗗𝗢𝗡𝗔𝗦𝗜 𝗕𝗢𝗦𝗤𝗨𝗘 ❉⊰━━✿
+┃  
+┣━⊱ *DANA*
+┣⊱ *[ TIDAK TERSEDIA ]*
+┣━⊱ *PULSA*
+┣⊱ *[ TIDAK TERSEDIA ]*
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃  *BOT BY  Mr ᭄B O O Tツ*
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
